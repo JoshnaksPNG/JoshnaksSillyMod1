ce9e4ae02f854d3604fe403d5b71055266c696e4
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2023-04-09 00:18:27 -0700

    Lego Recipe
    Added Lego Smelting Recipe
    
    Attempted Fix to Troll Animations

--- src/main/java/net/joshnaks/silly1/entity/custom/TrollfaceEntity.java
+++ src/main/java/net/joshnaks/silly1/entity/custom/TrollfaceEntity.java
@@ -1,37 +1,42 @@
 package net.joshnaks.silly1.entity.custom;
 
+import com.sun.jna.platform.win32.WinReg;
 import net.joshnaks.silly1.effect.ModEffects;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.InstancedAnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.AnimationController;
-import software.bernie.geckolib.core.animation.AnimationState;
-import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 import java.time.Instant;
 
 public class TrollfaceEntity extends HostileEntity implements GeoEntity
 {
-    private AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+    //private AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
-    private static final RawAnimation IDLE = RawAnimation.begin().thenLoop("animation.troll_face.idle");
-    private static final RawAnimation RUN = RawAnimation.begin().thenLoop("animation.troll_face.run");
-    private static final RawAnimation WALK = RawAnimation.begin().thenLoop("animation.troll_face.idle");
-    private static final RawAnimation ATTACK = RawAnimation.begin().thenPlay("animation.troll_face.attack");
+    private static final RawAnimation IDLE = RawAnimation.begin().then("animation.troll_face.idle", Animation.LoopType.LOOP);
+    private static final RawAnimation RUN = RawAnimation.begin().then("animation.troll_face.run", Animation.LoopType.LOOP);
+    private static final RawAnimation WALK = RawAnimation.begin().then("animation.troll_face.idle", Animation.LoopType.LOOP);
+    private static final RawAnimation ATTACK = RawAnimation.begin().then("animation.troll_face.attack", Animation.LoopType.PLAY_ONCE);
 
     private boolean inAttackAnimation = false;
     private long animationStart = Instant.now().toEpochMilli();
@@ -52,10 +57,11 @@ public class TrollfaceEntity extends HostileEntity implements GeoEntity
 
     private <E extends GeoAnimatable> PlayState predicate(AnimationState<E> event)
     {
+        //this.getDataTracker().startTracking( );
         if(this.isAttacking() && this.handSwinging && !inAttackAnimation)
         {
             event.getController().setAnimation(ATTACK);
-            inAttackAnimation = true;
+            animationStart = Instant.now().toEpochMilli();
             return PlayState.CONTINUE;
         }
 
@@ -70,7 +76,7 @@ public class TrollfaceEntity extends HostileEntity implements GeoEntity
             event.getController().setAnimation(IDLE);
         }
 
-        inAttackAnimation = animationStart + 334 > Instant.now().toEpochMilli();
+        inAttackAnimation = animationStart + 1200 > Instant.now().toEpochMilli();
 
         return PlayState.CONTINUE;
     }
@@ -83,6 +89,8 @@ public class TrollfaceEntity extends HostileEntity implements GeoEntity
         this.goalSelector.add(4, new LookAroundGoal(this));
 
         this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, MerchantEntity.class, true));
+        this.targetSelector.add(4, new ActiveTargetGoal<>(this, PassiveEntity.class, true));
 
     }
 
@@ -115,7 +123,12 @@ public class TrollfaceEntity extends HostileEntity implements GeoEntity
         super.onAttacking(target);
     }
 
+    @Override
+    public void tick()
+    {
 
+        super.tick();
+    }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
